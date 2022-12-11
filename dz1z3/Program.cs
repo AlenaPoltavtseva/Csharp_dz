c1b4f7e90b593b488d3d2d0d20e16ff053e1755c
Alena Poltavtseva <alena.panch@yandex.ru>
2022-12-11 23:27:09 +0300

    add dz1z3

---
+++ dz1z3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Вводите число");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Оно чётное");
+}
+else
+{
+    Console.WriteLine("Не чётное");
+}
