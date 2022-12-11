3ccd060e85bce694cb1ea27c799990b5d6715da7
Alena Poltavtseva <alena.panch@yandex.ru>
2022-12-11 23:13:01 +0300

    add dz1z2

---
+++ dz1z2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Вводите");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if ((b < c) & (c > max))
+{
+    max = c;
+}
+Console.WriteLine(max);
