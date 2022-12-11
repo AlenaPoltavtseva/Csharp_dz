8147758744e6e30f9619edfca38c47180a3fd460
Alena Poltavtseva <alena.panch@yandex.ru>
2022-12-11 20:28:49 +0300

    first commit

---
+++ dz1z1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Можете вводить");
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.Write(numberA);
+    Console.Write(" - Данное число больше, соответственно второе меньше");
+}
+else
+{
+    Console.Write(numberB);
+    Console.Write(" - Данное число больше, соответственно второе меньше");
+}
